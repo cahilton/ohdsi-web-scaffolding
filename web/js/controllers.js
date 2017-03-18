6a3c3a1bdbc43c70f8bb4692079517fdab112228
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2017-03-17 23:57:14 -0400

    hookup some fhir services

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,13 +1,65 @@
 angular.module('controllers', [])
-.controller('ohdsiInformerCtrl', ['$scope', 'ohdsiService', '$timeout', '$http', '$location',
-  function($scope, ohdsiService, $timeout, $http, $location) {
+.controller('ohdsiInformerCtrl', ['$scope', 'ohdsiService', 'fhirService', '$timeout', '$http', '$location',
+  function($scope, ohdsiService, fhirService, $timeout, $http, $location) {
 
   $scope.outcome = {};
   $scope.treatment = {};
   $scope.patient = {
-    nameLast : "Thomas",
-    nameFirst : "Louie",
-    sex : "Male",
-    age : "53"
+    id : 1
+  };
+
+
+  var getPatient = function(id) {
+    fhirService.getPatient(id)
+    .then(function(res) {
+      console.log(res);
+      $scope.patient = res;
+      fhirService.getMedications(id)
+        .then(function(res) {
+          $scope.patient.medications = res;
+          console.log(res);
+        }, function(err) {
+          console.log(err);
+        });
+      fhirService.getConditions(id)
+        .then(function(res) {
+          $scope.patient.conditions = res;
+          console.log(res);
+        }, function(err) {
+          console.log(err);
+        });
+    }, function(err) {
+      console.log('unabeld to read patient');
+    });
+  };
+
+  var getUrlParameter = function getUrlParameter(sParam) {
+      var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+          sURLVariables = sPageURL.split('&'),
+          sParameterName,
+          i;
+
+      for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split('=');
+
+          if (sParameterName[0] === sParam) {
+              return sParameterName[1] === undefined ? true : sParameterName[1];
+          }
+      }
+  };
+
+  var patientParam = getUrlParameter('patient');
+  if (patientParam) {
+    getPatient(patientParam);
+  } else {
+    getPatient(1);
+  }
+
+  $scope.medicationClicked = function(item) {
+
+  };
+
+  $scope.conditionClicked = function(item) {
+
   };
 }]);
--- web/js/services.js
+++ web/js/services.js
@@ -1,4 +1,68 @@
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
 angular.module('services', [])
 .service('ohdsiService', ['$http', '$q', function($http, $q){
-  console.log("ohdsiService");
+
+}])
+.service('fhirService', ['$http', '$q', function($http, $q){
+  var self = this;
+  this.getConditions = function(patientId) {
+    var deferred = $q.defer();
+    $http.get('http://ec2-54-70-205-229.us-west-2.compute.amazonaws.com:8080/gt-fhir-webapp/base/Condition?patient=' + patientId)
+    .then(function(res) {
+      deferred.resolve(res.data.entry);
+    }, function(err) {
+      console.log(response);
+      deferred.reject(repsonse);
+    });
+
+    return deferred.promise;
+  };
+
+  this.getMedications = function(patientId) {
+    var deferred = $q.defer();
+    $http.get('http://ec2-54-70-205-229.us-west-2.compute.amazonaws.com:8080/gt-fhir-webapp/base/MedicationDispense?patient=' + patientId)
+    .then(function(res) {
+      deferred.resolve(res.data.entry);
+    }, function(err) {
+      console.log(response);
+      deferred.reject(repsonse);
+    });
+    return deferred.promise;
+  };
+
+  this.getPatient = function(patientId) {
+    var deferred = $q.defer();
+    $http.get('http://ec2-54-70-205-229.us-west-2.compute.amazonaws.com:8080/gt-fhir-webapp/base/Patient/' + patientId)
+    .then(function(res) {
+      var d = res;
+      d.id = patientId;
+      if (d.data) {
+        var birthDate = Date.parse(d.data.birthDate);
+        d.age = self.getAge(birthDate);
+        d.nameLast = toTitleCase(d.data.name[0].family[0]);
+        d.nameFirst = toTitleCase(d.data.name[0].given[0]);
+        d.sex = toTitleCase(d.data.gender);
+      }
+      deferred.resolve(d);
+    }, function(err) {
+      console.log(response);
+      deferred.reject(repsonse);
+    });
+    return deferred.promise;
+  };
+
+  this.getAge = function(birthDate) {
+    var today = new Date();
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+  }
+
+
 }]);
